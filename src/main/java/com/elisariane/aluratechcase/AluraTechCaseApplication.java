21f6ba1cadd864975c87bd2af2f4d0a5f3c4ee3c
Elisariane <elisarianeBS@gmail.com>
2024-03-20 12:51:16 -0300

    feat: Initial project setup

---
+++ src/main/java/com/elisariane/aluratechcase/AluraTechCaseApplication.java
@@ -0,0 +1,13 @@
+package com.elisariane.aluratechcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraTechCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraTechCaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elisariane/aluratechcase/AluraTechCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elisariane.aluratechcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraTechCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
