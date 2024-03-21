cfbd9a0268171a6f0d1729e060550c2694dec114
Elisariane <elisarianeBS@gmail.com>
2024-03-21 16:04:30 -0300

    feat: Add endpoint get user by username for ADMIN users

--- src/main/java/com/elisariane/aluratechcase/controllers/UserController.java
+++ src/main/java/com/elisariane/aluratechcase/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.elisariane.aluratechcase.controllers;
 
 import com.elisariane.aluratechcase.domain.user.User;
+import com.elisariane.aluratechcase.domain.user.UserListData;
 import com.elisariane.aluratechcase.domain.user.UserRegistrationData;
 import com.elisariane.aluratechcase.repositories.UserRepository;
 import jakarta.transaction.Transactional;
@@ -8,22 +9,19 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
-@RequestMapping("user")
 public class UserController {
 
     @Autowired
     private UserRepository repository;
 
-    @PostMapping
+    @PostMapping("/user")
     @Transactional
     public ResponseEntity<String> createUser(@RequestBody @Valid UserRegistrationData userRegistrationData) {
         Optional<User> existingUser = repository.getUserByUsername(userRegistrationData.username());
@@ -39,5 +37,16 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body("User created successfully with ID: " + newUser.getId());
     }
 
+    @GetMapping("/users/{username}")
+    @Secured("ADMIN")
+    public ResponseEntity<UserListData> getUserByUsername(@PathVariable String username) {
+        var user = repository.getUserByUsername(username);
+
+        if (user.isPresent()) {
+            return  ResponseEntity.ok().body(new UserListData(user.get()));
+        }
+
+       return ResponseEntity.notFound().build();
+    }
 
 }
--- src/main/java/com/elisariane/aluratechcase/domain/user/UserListData.java
+++ src/main/java/com/elisariane/aluratechcase/domain/user/UserListData.java
@@ -1,4 +1,8 @@
 package com.elisariane.aluratechcase.domain.user;
 
 public record UserListData(String username, String email, Role role) {
+
+    public UserListData(User user) {
+        this(user.getUsername(), user.getEmail(),  user.getRole());
+    }
 }
